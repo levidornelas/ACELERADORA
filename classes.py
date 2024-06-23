b41ff5156c686740f73d5f1c05e9f61f160a4f62
Levi <165521979+levidornelas@users.noreply.github.com>
2024-06-22 21:50:31 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+
 # Listas vazias, a serem preenchidas com os inputs do usuário
 pacientes_cadastrados = []
 agendamentos = []
@@ -12,23 +13,28 @@ class Paciente:
     def cadastrar_paciente():
         nome = input("Digite o nome do paciente: ")
         telefone = input("Digite o telefone do paciente: ")
+
         telefone = telefone.strip()
-        
-        # Tratamento de erro: verifica se já existe um número de telefone cadastrado.
+
+        # Verifica se o telefone tem 9 dígitos e se é numérico
+        if len(telefone) != 9 or not telefone.isdigit():
+            print('Por favor insira um número de telefone válido. O telefone deve conter exatamente 9 dígitos.')
+            return
+
+        # Verifica se já existe um número de telefone cadastrado
         for paciente in pacientes_cadastrados:
             if paciente.telefone == telefone:
                 print("Paciente já cadastrado!")
-            if len(telefone) != 9 or not telefone.isdigit():
-                print('Por favor insira um número de telefone válido. O telefone deve conter exatamente 9 dígitos.')
-                return            
-        #Chamando a classe Paciente, e também inserindo os inputs dentro da lista:
+                return
+
+        # Chamando a classe Paciente, e também inserindo os inputs dentro da lista:
         paciente = Paciente(nome, telefone)
         pacientes_cadastrados.append(paciente)
         print("Paciente cadastrado com sucesso!")
 
     def listar_pacientes():
         for i, paciente in enumerate(pacientes_cadastrados, start=1):
-            print(f"{i}. {paciente.nome} ({paciente.telefone})")
+            print(f"{i}. Nome: {paciente.nome} Telefone: ({paciente.telefone})")
 
     def marcar_consulta():
         Paciente.listar_pacientes()
@@ -53,7 +59,7 @@ class Paciente:
         consulta = Consulta(paciente, data, hora, especialidade)
         agendamentos.append(consulta)
         print("Consulta agendada com sucesso!")
-        
+
 class Consulta:
     def __init__(self, paciente, data, hora, especialidade):
         self.paciente = paciente
@@ -61,11 +67,11 @@ class Consulta:
         self.hora = hora
         self.especialidade = especialidade
 
-    #Função auxiliar de 'cancelar_consulta'
+    # Função auxiliar de 'cancelar_consulta'
     def listar_agendamentos():
         for i, consulta in enumerate(agendamentos, start=1):
             print(f"{i}. {consulta.paciente.nome} - {consulta.data} às {consulta.hora} ({consulta.especialidade})")
-    
+
     def cancelar_consulta():
         Consulta.listar_agendamentos()
         escolha = int(input("Escolha o número da consulta para cancelar: "))
@@ -82,19 +88,20 @@ class Consulta:
         if confirmacao.lower() == "s":
             agendamentos.remove(consulta)
             print("Consulta cancelada com sucesso!")
-    #Salvando os dados num arquivo JSON, depois do programa ser encerrado com a opção '4':
+
+    # Salvando os dados num arquivo JSON, depois do programa ser encerrado com a opção '4':
     def salvar_dados():
         dados = {
-                'pacientes': [paciente.__dict__ for paciente in pacientes_cadastrados],
-                'consultas': [{
+            'pacientes': [paciente.__dict__ for paciente in pacientes_cadastrados],
+            'consultas': [{
                 'paciente': consulta.paciente.__dict__,
                 'data': consulta.data,
                 'hora': consulta.hora,
                 'especialidade': consulta.especialidade
-                } for consulta in agendamentos]
-            }
+            } for consulta in agendamentos]
+        }
 
-        caminho_arquivo = 'C:/Users/PC/Desktop/ACELERADORA/banco.json'  #Definindo o caminho com o path inteiro do arquivo, para evitar erros.
-        
+        caminho_arquivo = 'C:/Users/PC/Desktop/ACELERADORA/banco.json'  # Definindo o caminho com o path inteiro do arquivo, para evitar erros.
+
         with open(caminho_arquivo, 'w') as arquivo_json:
             json.dump(dados, arquivo_json, indent=2)
